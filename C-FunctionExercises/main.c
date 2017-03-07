1da9bfe3109f00b145d563a598648e9444afa812
dshardy <darren_hardy@icloud.com>
2017-03-07 22:45:54 +0000

    Initial Commit

---
+++ C-FunctionExercises/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-FunctionExercises
+//
+//  Created by Darren Hardy on 07/03/2017.
+//  Copyright © 2017 Darren Hardy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
